d1bbc17d97ad6660ac4a1aea665afc4daf10a633
Alex(ia) Lee <alexia.lee@uqconnect.edu.au>
2015-02-06 14:35:23 -0800

    Added a function to split image up into grids of size N

--- motion_detection/src/main.cpp
+++ motion_detection/src/main.cpp
@@ -9,14 +9,16 @@
 #include <string>
 #include <algorithm>
 #include <sstream>
+#include <vector>
 
 // OpenCV Includes 
-#include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 // ROS Includes 
 #include <ros/ros.h>
+// Provides interface between ROS images and OpenCV images
+#include <cv_bridge/cv_bridge.h>
 
 // iRat messages
 #include <irat_msgs/IRatVelocity.h>
@@ -37,6 +39,52 @@ namespace enc = sensor_msgs::image_encodings;
 // Name for window that displays camera feed
 static const char iRatView[] = "iRat's View";
 
+
+/**
+ * Split the image into NxN grids. 
+ *
+ * @param N the dimensions of the grid 
+ * @param orig the original Mat image to be split up. 
+ *
+ * @return vector<cv::Mat> 
+ */
+vector<Mat> split(int N, Mat orig) {
+  vector<Mat> grids;
+  int numGridRows = orig.rows/N;
+  int numGridCols = orig.cols/N;
+  int leftOverRowPix = orig.rows % N;
+  int leftOverColPix = orig.cols % N;
+  int topBuffer = leftOverRowPix/2;
+  int leftBuffer = leftOverColPix/2;
+ 
+  /*
+  cout << "numGridRows: " << numGridRows << endl;
+  cout << "numGridCols: " << numGridCols << endl;
+  cout << "topBuffer: " << topBuffer << endl;
+  cout << "leftBuffer: " << leftBuffer << endl;
+  cout << "rows: " << orig.rows << endl;
+  cout << "cols: " << orig.cols << endl;
+  */
+  
+  // If we can't evenly divide up the grid we'll try to 
+  // centre the image, and cut off the left-over pixels on
+  // the edges. 
+  for(int i=0; i < numGridRows; i++) {
+    int rowStart = topBuffer + N*i;
+    for(int j=0; j < numGridCols; j++) {
+      int colStart = leftBuffer + N*j;
+      // Make and add the grid 
+      /*
+      cout << "rowStart: " << rowStart << endl;
+      cout << "colStart: " << colStart << endl;
+      */
+      grids.push_back(Mat (orig, Rect(colStart, rowStart, N, N)));
+    }
+  }
+
+  return grids;
+}
+
 /**
  * Callback for images sent from iRat's camera.
  */
@@ -50,9 +98,16 @@ void image_callback(sensor_msgs::ImageConstPtr image) {
     return;
   }
 
-  // show image
-  cout << "Showing image..." << endl;
+  // CvImagePtr has a cv::Mat image member.
   imshow(iRatView, cvPtr->image);
+
+  Mat cvImage = cvPtr->image;
+  vector<Mat> grids = split(4, cvImage);
+  /*
+  for(int i = 0; i < grids.size(); i++) {
+    cout << grids[i] << endl;
+  }
+  */
 }
 
 int main(int argc, char** argv) {
@@ -73,7 +128,7 @@ int main(int argc, char** argv) {
   ros::NodeHandle node("~");
   
   // set topic root from launch file/command line
-  node.param("topic", topic, string("/irat_red"));
+  node.param("topic", topic, string("/" + irat_id));
   
   cout << "Creating window" << endl;
 
