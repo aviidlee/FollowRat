3b31a054d10c6b04d9044a34fd87fd9eef34168d
Alex(ia) Lee <alexia.lee@uqconnect.edu.au>
2015-02-09 16:35:18 -0800

    Code for getting images from webcam

--- overhead_camera/main.cpp
+++ overhead_camera/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <string>
+
+#define EVER ;;
 
 using namespace cv;
+using namespace std;
 
-int main(int, char**) {
-	VideoCapture cap(1); // hopefully open the webcam; 0 is default.
+int main(int argc, char** argv) {
+  int deviceNo;
+
+  if(argc == 2) {
+    deviceNo = atoi(argv[1]);
+  } else {
+    cout << "Usage: overhead <deviceNumber>" << endl;
+    return -1;
+  }
+
+	VideoCapture cap(deviceNo); // hopefully open the webcam; 0 is default.
 	if(!cap.isOpened()) {
 		return -1;
 	}
-
+  
+  cout << "YAY" << endl;
 	cvNamedWindow("Webcam", 1);
 
-	for(;;) {
+	for(EVER) {
 		Mat frame;
 		cap >> frame; // get new frame from camera
 		imshow("Webcam", frame);
