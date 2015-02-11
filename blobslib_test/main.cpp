883bf1bb209a9c36c38a655d666a9561282c6202
Alex(ia) Lee <alexia.lee@uqconnect.edu.au>
2015-02-11 11:18:04 -0800

    Can get blobslib to compile and link. For installation: clone the git repo, go into it. Create a build directory. Inside the build directory, type something like cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..and then do make and sudo make install.

---
+++ blobslib_test/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * FILE: std.cpp
+ * AUTHOR: avadendas
+ * DESCRIPTION: The most vanilla cpp template. 
+ */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+// Blobs lib
+#include <BlobResult.h>
+  
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+  cout << "Hello blobs!" << endl;
+  return 0;
+}
